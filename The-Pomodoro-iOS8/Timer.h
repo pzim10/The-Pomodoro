fbed369ede4077d07ad0a5e9e87a3aeeb9be5adf
Taylor Mott <trm36@humboldt.edu>
2015-05-11 14:50:28 -0600

    Update from Round to TimerCompleteNotification
    Signed-off-by: Taylor Mott <trm36@humboldt.edu>

--- The-Pomodoro-iOS8/Timer.h
+++ The-Pomodoro-iOS8/Timer.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 static NSString *SecondTickNotification = @"secondTickNotification";
-static NSString *TimerCompleteNotification = @"roundCompleteNotification";
+static NSString *TimerCompleteNotification = @"timerCompleteNotification";
 static NSString *NewRoundNotification = @"newRoundNotification";
 
 @interface Timer : NSObject
