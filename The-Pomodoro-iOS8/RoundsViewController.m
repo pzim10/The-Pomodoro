c1d9f1b5418577a22629fa69255125a503a6c61e
Peter Zimmerman <pzim10@gmail.com>
2015-07-09 16:26:01 -0600

    RoundsViewController changes

--- The-Pomodoro-iOS8/RoundsViewController.m
+++ The-Pomodoro-iOS8/RoundsViewController.m
@@ -2,21 +2,47 @@
 //  RoundsViewController.m
 //  The-Pomodoro-iOS8
 //
-//  Created by Peter Zimmerman on 7/9/15.
+//  Created by Taylor Mott on 18.2.2015.
 //  Copyright (c) 2015 DevMountain. All rights reserved.
 //
 
 #import "RoundsViewController.h"
+#import "RoundsVC.h"
+#import "Timer.h"
 
-@interface RoundsViewController ()
+static NSString *reuseID = @"reuseID";
+
+@interface RoundsViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) UITableView *tableView;
 
 @end
 
 @implementation RoundsViewController
 
+- (instancetype)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        [self registerForNotifications];
+    }
+    
+    return self;
+}
+
+//Set the tableView delegate and datasource to self.
+//register the class
+//add tableview as the subview of the viewControllers view
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:reuseID];
+    [self.view addSubview:self.tableView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +50,85 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+//Unregister for notifications
+- (void)dealloc
+{
+    [self unregisterForNotifications];
 }
-*/
+
+#pragma mark - TableView Delegate Methods
+
+//Set the currentRound for the index selected
+//call the roundSelected method which sets the minutes and seconds accordingly
+//Cancel the timer
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [RoundsVC sharedInstance].currentRound = indexPath.row;
+    [[RoundsVC sharedInstance] roundSelected];
+    [[Timer sharedInstance] cancelTimer];
+}
+
+#pragma mark - TableView DataSource Methods
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:reuseID];
+    
+    NSArray *roundsArray = [RoundsVC sharedInstance].roundTimes;
+    NSNumber *minutes = roundsArray[indexPath.row];
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%li minutes", (long)[minutes integerValue]];
+    
+    return cell;
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [RoundsVC sharedInstance].roundTimes.count;
+}
+
+#pragma mark - Notification Methods
+
+//Use the notficationCenter to register for the notification
+- (void)registerForNotifications
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(roundComplete) name:TimerCompleteNotification object:nil];
+}
+
+//Compare the current round to the roundTimes.count to see if there are still times left to be ran.
+//If there are still times left add one to current round and then update the time.
+//Otherwise set currentRound to 0
+- (void)roundComplete
+{
+    if ([RoundsVC sharedInstance].currentRound < [RoundsVC sharedInstance].roundTimes.count - 1)
+    {
+        [RoundsVC sharedInstance].currentRound++;
+        [self.tableView selectRowAtIndexPath:[NSIndexPath indexPathForRow:[RoundsVC sharedInstance].currentRound inSection:0] animated:YES scrollPosition:UITableViewScrollPositionNone];
+        [[RoundsVC sharedInstance] roundSelected];
+    }
+    else
+    {
+        [RoundsVC sharedInstance].currentRound = 0;
+        [self.tableView selectRowAtIndexPath:[NSIndexPath indexPathForRow:[RoundsVC sharedInstance].currentRound inSection:0] animated:YES scrollPosition:UITableViewScrollPositionNone];
+        [[RoundsVC sharedInstance] roundSelected];
+    }
+}
+
+
+//Unregister the notifications
+- (void)unregisterForNotifications
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
